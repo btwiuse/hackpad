f19bfcd6b65eadb917a97c70033d2f343d413891
Broken Pipe <navigaid@gmail.com>
2026-05-19 18:14:11 +0800

    add init.wasm

---
+++ cmd/init/main.go
@@ -0,0 +1,27 @@
+//go:build js && wasm
+
+package main
+
+import (
+	"time"
+	"syscall/js"
+
+	"github.com/hack-pad/hackpad/internal/install"
+	"github.com/hack-pad/hackpad/internal/interop"
+	"github.com/hack-pad/hackpad/internal/js/fs"
+	"github.com/hack-pad/hackpad/internal/js/process"
+	"github.com/hack-pad/hackpad/internal/global"
+)
+
+func init() {
+	process.Init()
+	fs.Init()
+	global.Set("install", js.FuncOf(install.InstallFunc))
+	interop.SetInitialized()
+
+	time.Sleep(time.Second)
+}
+
+func main() {
+	select{}
+}
